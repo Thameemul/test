477007375a8997512f18f74de9e3bdb060abf9ea
Thameemul Anzari <thameemul.anzari@ministrybrands.com>
2020-06-25 11:37:31 +0530

    Removed unwanted spaces

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ namespace WebApplication3
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            
-            
-            
-            
         }
 
         public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
